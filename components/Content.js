e1b2439e9e8fb2cba102c789303d7b6f1501e8fa
Alex Chao <alexchao2022@u.northwestern.edu>
2021-01-16 19:37:55 -0600

    CocktailList created

--- components/Content.js
+++ components/Content.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { ListItem, Avatar } from 'react-native-elements'
+import CocktailList from './CocktailList/index.js'
 
 
 export default class Content extends Component {
@@ -12,36 +13,27 @@ export default class Content extends Component {
         }*/
     }
 
+    //Fetch cocktail data.
+
     render () {
         const list = [
-        {
-            name: 'Classic Margarita',
-            avatar_url: 'https://i1.wp.com/www.moodymixologist.com/wp-content/uploads/2020/02/classic-margarita-cocktail-recipe-03-3560629.jpg?w=733&ssl=1',
-            ingredients: 'ingredients'
-        },
-        {
-            name: 'Chris Jackson',
-            avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-            ingredients: 'ingredients'
-        },
+            {
+                name: 'Classic Margarita',
+                avatar_url: 'https://i1.wp.com/www.moodymixologist.com/wp-content/uploads/2020/02/classic-margarita-cocktail-recipe-03-3560629.jpg?w=733&ssl=1',
+                ingredients: 'ingredients'
+            },
+            {
+                name: 'Chris Jackson',
+                avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+                ingredients: 'ingredients'
+            },
         ]
 
         return (
-            <div>
-                <View>
-                {
-                list.map((l, i) => (
-                    <ListItem key={i} bottomDivider>
-                    <Avatar source={{uri: l.avatar_url}} />
-                    <ListItem.Content>
-                        <ListItem.Title>{l.name}</ListItem.Title>
-                        <ListItem.Subtitle>{l.ingredients}</ListItem.Subtitle>
-                    </ListItem.Content>
-                    </ListItem>
-                ))
-                }
-                </View>
-            </div>
+            <View>
+                <Text>This is our BarBuddy App!</Text>
+                <CocktailList/>
+            </View>
         )
     }
 }
