520f612f448ef03b7095d36942c7e9d7eec34995
mKol13134 <61067214+mkolowrat@users.noreply.github.com>
2021-01-16 18:11:33 -0800

    sat work

--- components/CocktailList/index.js
+++ components/CocktailList/index.js
@@ -7,23 +7,11 @@ export default class CocktailList extends Component {
 
 
     render () {
-        const list = [
-            {
-                name: 'Classic Margarita',
-                avatar_url: 'https://i1.wp.com/www.moodymixologist.com/wp-content/uploads/2020/02/classic-margarita-cocktail-recipe-03-3560629.jpg?w=733&ssl=1',
-                ingredients: 'ingredients'
-            },
-            {
-                name: 'Chris Jackson',
-                avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-                ingredients: 'ingredients'
-            },
-        ]
-
+        
         return (
             <View>
                 {
-                    list.map((l, i) => (
+                    this.props.cocktailList.map((l, i) => (
                         <ListItem key={i} bottomDivider>
                         <Avatar source={{uri: l.avatar_url}} />
                         <ListItem.Content>
--- components/Content.js
+++ components/Content.js
@@ -7,32 +7,34 @@ import CocktailList from './CocktailList/index.js'
 export default class Content extends Component {
     constructor(props) {
         super(props);
-
-        /*this.state = {
-
-        }*/
+        this.state = {
+            cocktailList: []
+        }
     }
 
     //Fetch cocktail data.
+    // pass cocktail info to cocktail componenet 
+    // take cocktail componenet and input into cocktailist 
 
-    render () {
-        const list = [
-            {
-                name: 'Classic Margarita',
-                avatar_url: 'https://i1.wp.com/www.moodymixologist.com/wp-content/uploads/2020/02/classic-margarita-cocktail-recipe-03-3560629.jpg?w=733&ssl=1',
-                ingredients: 'ingredients'
-            },
-            {
-                name: 'Chris Jackson',
-                avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-                ingredients: 'ingredients'
-            },
-        ]
+    getCocktailData() {
+        this.state.cocktailList = [{
+            name: 'Classic Margarita',
+            avatar_url: 'https://i1.wp.com/www.moodymixologist.com/wp-content/uploads/2020/02/classic-margarita-cocktail-recipe-03-3560629.jpg?w=733&ssl=1',
+            ingredients: 'ingredients'
+        },
+        {
+            name: 'Chris Jackson',
+            avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+            ingredients: 'ingredients'
+        },]
+    }
 
+    render () {   
+        this.getCocktailData() 
         return (
             <View>
                 <Text>This is our BarBuddy App!</Text>
-                <CocktailList/>
+                <CocktailList cocktailList = {this.state.cocktailList}/>
             </View>
         )
     }
